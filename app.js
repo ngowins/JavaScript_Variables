db7a577245c47871784691465be1f2a8f13547d7
Nate Gowins <ngowins@gmail.com>
2022-03-22 14:23:33 -0500

    Completed exercises

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Nate";
+let lastName = "Gowins";
+var age = 30;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 
 // Exercise 3
+let favorite = "cycling";
+
+let city = "Chelsea";
+
+let myStory = `Hello! My name is ${fullName}. I currently live in ${city}. My favorite pastime is ${favorite} and learning to code.`;
+
+console.log(myStory);
